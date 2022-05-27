d0575075cbcd738d1c8439a9d96d6d008d3b3beb
Roberto Bressanelli <rbressanelli@gmail.com>
2022-05-27 17:38:56 -0300

    Feat: code progress

--- payments/serializers.py
+++ payments/serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 from .models import Payment
 
 
-class PaymentSerializer(serializers.ModelSerializer):
-    model = Payment
-    fields = '__all__'
+class PaymentSerializer(serializers.Serializer):
+    id = serializers.UUIDField(read_only=True)
+    amount = serializers.CharField()
+    late_fee_per_day = serializers.FloatField()
+    payment_date = serializers.DateField()
+    
+    rental_id = serializers.UUIDField()
+    user_id = serializers.UUIDField()
 
--- rentals/views.py
+++ rentals/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView, UpdateAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
@@ -23,16 +24,12 @@ class RentalView(ListAPIView):
     serializer_class = RentalSerializer
 
 
-class CloseRentalView(UpdateAPIView):
+class CloseRentalView(APIView):
     
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAdminUser]
-    
-    queryset = Rental.objects.all()
-    serializer_class = CloseRentalSerializer
 
-
-    def update(self, request, pk, *args, **kwargs):
+    def put(self, request, pk):
         
         rental = Rental.objects.filter(pk=pk).first()        
         rental.return_date = request.data['return_date']
@@ -52,23 +49,28 @@ class CloseRentalView(UpdateAPIView):
         
         amount = returned_date_no_fee.days * media.rental_price_per_day          
                
-        amount = amount + fee
-        print(f'{amount:.2f}')
+        amount = amount + fee        
         # user.rental_active = False        
         # user.save()
         # media.available = False
         # media.save()        
-        
+  
         request.data['amount'] = f'{amount:.2f}'
-        request.data['late_fee_per_day'] = request.data['late_fee_per_day']
         request.data['payment_date'] = returned_date
+        request.data['rental_id'] = rental.id
+        request.data['user_id'] = user.id
         
         serializer = PaymentSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        # if not serializer.is_valid():
-        #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        # print(serializer.data)
-        # payment = Payment.objects.create(**serializer.validated_data)
         
+        # serializer.is_valid(raise_exception=True)
+        if not serializer.is_valid():
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-        return Response({}, status=status.HTTP_200_OK)
+        payment = Payment.objects.create(**serializer.validated_data)        
+        serializer = PaymentSerializer(payment)
+        
+        rental.payment = payment
+
+        rental_serializer = RentalSerializer(rental)
+        
+        return Response({rental_serializer.data}, status=status.HTTP_200_OK)
