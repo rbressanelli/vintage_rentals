0b9cb8d6524aee9765e548c68a17d37052dbfcc7
Roberto Bressanelli <rbressanelli@gmail.com>
2022-05-30 10:40:56 -0300

    Refactor: changed relation logic with payment model and serializer

--- payments/models.py
+++ payments/models.py
@@ -7,8 +7,6 @@ class Payment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     amount = models.FloatField(null=False)
     late_fee_per_day = models.FloatField(default=0)
-    payment_date = models.DateField(null=False)
-    
-    rental = models.OneToOneField('rentals.Rental', on_delete=models.CASCADE, null=True)
+    payment_date = models.DateField(null=False)    
     
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='payments')
--- payments/serializers.py
+++ payments/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers
 
-from .models import Payment
-
-
 class PaymentSerializer(serializers.Serializer):
     id = serializers.UUIDField(read_only=True)
     amount = serializers.CharField()
     late_fee_per_day = serializers.FloatField()
-    payment_date = serializers.DateField()
-    
-    rental_id = serializers.UUIDField()
-    user_id = serializers.UUIDField()
-
+    payment_date = serializers.DateField()  
