4483b75b5703134549ce8d5e0f96be58c3c53b65
Roberto Bressanelli <rbressanelli@gmail.com>
2022-06-02 10:45:10 -0300

    Refactor: deleted unused code

--- addresses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from addresses.views import addresses
-
-urlpatterns =[ 
-    path("get_adress/", ),
-    path("post_adress/", ),
-    path("update_adress/",),
-    path("delet_adress/",)]
